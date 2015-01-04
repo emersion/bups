6669d133800300c2c09964f4c7a3625f3be29e4a
Emersion <contact@emersion.fr>
2015-01-04 14:32:20 +0100

    Updates tools/makedeb.sh

--- tools/makedeb.sh
+++ tools/makedeb.sh
@@ -4,4 +4,5 @@ dirname=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 setup="python2 $dirname/../setup.py"
 
 rm -rf "$dirname/../deb_dist"
+$dirname/../tools/makemo.sh
 $setup --command-packages=stdeb.command bdist_deb
