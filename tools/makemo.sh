f82f653cf638b4afb431b61a94d23b183d18db35
emersion <contact@emersion.fr>
2016-01-03 19:15:17 +0100

    Fixes tools/makemo.sh

--- tools/makemo.sh
+++ tools/makemo.sh
@@ -2,4 +2,6 @@
 
 dirname=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-msgfmt "$dirname/../locale/fr/LC_MESSAGES/bups.po" -o "$dirname/../locale/fr/LC_MESSAGES/bups.mo"
+for langdir in $(echo "$dirname/../locale/*/"); do
+	msgfmt "$langdir/LC_MESSAGES/bups.po" -o "$langdir/LC_MESSAGES/bups.mo"
+done
